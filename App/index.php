c4a1b26f5aca22a3d9616edf0f12b5209e440472
FranciscoH27 <franuleg@gmail.com>
2024-06-19 23:30:36 -0300

    Interfaces

--- App/index.php
+++ App/index.php
@@ -1,11 +1,24 @@
 <?php
 
-use Slim\Factory\AppFactory;
+error_reporting(-1);
+ini_set('display_errors', 1);
 
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
+
+require_once './controller/UsuarioController.php';
 require_once '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 $app = AppFactory::create();
 
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/name', function ($request, $response, array $args) {
 		$response->getBody()->write("Funciona!");
